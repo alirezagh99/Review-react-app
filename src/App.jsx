b5937e6cca287ffbd63b1ed5c05898a4ffd785ed
Alireza Ghasemi <agh102724@gmail.com>
2024-12-26 18:58:51 +0330

    add-Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import Card from "./components/Card";
+import data from "./data";
 function App() {
+  const [index, setIndex] = useState(0);
+  console.log(data[index]);
   return (
     <>
-      <Card />
+      <Card data={data[index]} />
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
-function Card() {
-  return <div>Card</div>;
+function Card(props) {
+  const { id, text, name, job, image } = props.data;
+  console.log(text);
+  return (
+    <div className="w-1/2 m-auto shadow-2xl p-5 flex mt-16 flex-col items-center">
+      <img
+        className="rounded-full h-[150px] w-[150px] mb-3"
+        src={image}
+        alt={name}
+      />
+
+      <div className="text-center">
+        <h2 className="text-xl font-semibold">{name}</h2>
+        <div className="text-[rgba(0,0,0,0.7)] text-sm mb-4">{job}</div>
+        <p className="text-[rgba(0,0,0,0.75)] mb-4 text-sm">{text}</p>
+      </div>
+      <div className="mb-4">
+        <button className="border-2 hover:bg-black hover:text-white transition-all duration-500 rounded px-2 m-3">
+          prev
+        </button>
+        <button className="border-2 hover:bg-black hover:text-white transition-all duration-500 rounded px-2 m-3">
+          next
+        </button>
+      </div>
+      <button className="border hover:bg-black hover:text-white transition-all duration-500 px-3 text-sm bg-gray-200 text-blue-500">
+        Surprise me
+      </button>
+    </div>
+  );
 }
 
 export default Card;
